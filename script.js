8936bc506e420b85c609810df6905dcb571a5c59
Raamgopal <142202310+Raamgopal8@users.noreply.github.com>
2024-02-04 18:54:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contactBtn').addEventListener('click', function () {
-    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
-});
-
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Message sent! We will get back to you soon.');
-    // You can add backend logic to handle the form data.
-});
