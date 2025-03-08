13a3d1d920b7f67deebeff157a75f987d023731b
Raamgopal <142202310+Raamgopal8@users.noreply.github.com>
2025-03-08 11:41:58 +0530

    NEW

---
+++ WEEK1/server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM(`<!DOCTYPE html>`);
+const document = dom.window.document;
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = process.env.PORT ;
+const path  = require('path')
+
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection
+db.once('open',()=>{
+    console.log("mongodb connection success!")
+})
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, match: /.+\@.+\..+/ }, // Basic email validation
+  message: { type: String, required: true }
+})
+const Users = mongoose.model("data",userSchema)
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'Public','index.html'))
+})
+
+app.post('/post',async(req,res)=>{
+  
+  const {name,email,message} = req.body;
+
+  const user = new Users({
+    name,
+    email,
+    message
+  })
+  await user.save()
+  console.log(user)
+  res.sendFile(path.join(__dirname,'Public','index.html'))
+})
+
+
+app.listen(port,()=>{
+    console.log("Server is Running")
+})
+
+//end of the code 
